34125465848016bbabe22995f4c10daf3957cfc9
pavanabburi09 <pavanabburi09@gmail.com>
2022-03-18 20:56:14 -0300

    Bug fixes and reroute implementation in header

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer fixed-bottom" style={{ background: "#dd7230" }}>
+    <footer style={{ background: "#dd7230" }}>
       <div className="container text-center">
         <button className="btn btn-md text-white">Contact Us</button>
         <button className="btn btn-md text-white">About Us</button>
