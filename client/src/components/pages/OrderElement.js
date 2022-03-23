45a92f9a02d1d2ab3656a86a3b993779043c3e24
Aadil <ad979991@dal.ca>
2022-03-23 10:11:12 -0300

    User status tracking completed

--- client/src/components/pages/OrderElement.js
+++ client/src/components/pages/OrderElement.js
@@ -1,41 +1,78 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import {Button} from "react-bootstrap"
+import { Button } from "react-bootstrap";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 function OrderElement(props) {
   const navigator = useNavigate();
+  const { image_url, dish_name, quantity, order_status } = props;
   return (
     <>
-      <div class="container1">
-        <div class="Image">
-          <img
-            align="right"
-            width="300px"
-            height="100%"
-            src="https://www.cookwithmanali.com/wp-content/uploads/2019/05/Paneer-Butter-Masala.jpg"
-          ></img>
-        </div>
-        <div class="Order-Details">
-          <h3>Paneer Tikka Masala</h3>
-          <h3>Quantity : 2</h3>
-          <h3>Order Status:</h3>
-          <h3>- Order Confirmed.</h3>
-          <h3>- Delivery boy has picked up your order.</h3>
-        </div>
-        <div class="Button">
-          <Button
-            type="button"
-            align="center"
-            onClick={() => {
-              navigator(-1);
-            }}
-          >
-            Go back!
-          </Button>
-        </div>
+      <div class="content" align="center">
+        <Card sx={{ maxWidth: 300, maxHeight: 900, m: 2 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="170"
+              image={image_url}
+              alt="Image"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {dish_name}
+              </Typography>
+              {/* variant="body2" color="text.secondary" */}
+              <Typography>Quantity : {quantity}</Typography>
+              <Typography>
+                Order Status:{" "}
+                <Typography variant="body" color="text.primary">
+                  {" "}
+                  {order_status}
+                </Typography>
+              </Typography>
+            </CardContent>
+            <CardActions style={{ justifyContent: "center" }}></CardActions>
+          </CardActionArea>
+        </Card>
       </div>
     </>
   );
 }
 
 export default OrderElement;
+
+// <>
+//   <div class="container1">
+//     <div class="Image">
+//       <img
+//         align="right"
+//         width="300px"
+//         height="100%"
+//         src="https://www.cookwithmanali.com/wp-content/uploads/2019/05/Paneer-Butter-Masala.jpg"
+//       ></img>
+//     </div>
+//     <div class="Order-Details">
+//       <h3>Paneer Tikka Masala</h3>
+//       <h3>Quantity : 2</h3>
+//       <h3>Order Status:</h3>
+//       <h3>- Order Confirmed.</h3>
+//       <h3>- Delivery boy has picked up your order.</h3>
+//     </div>
+//     <div class="Button">
+//       <Button
+//         type="button"
+//         align="center"
+//         onClick={() => {
+//           navigator(-1);
+//         }}
+//       >
+//         Go back!
+//       </Button>
+//     </div>
+//   </div>
+// </>
--- client/src/components/pages/OrderStatus.js
+++ client/src/components/pages/OrderStatus.js
@@ -2,12 +2,43 @@ import React from "react";
 import "./css/orderstatus.css";
 import { useNavigate } from "react-router-dom";
 import OrderElement from "./OrderElement";
+import Grid from "@mui/material/Grid";
+import { borders } from "@mui/system";
+import { Button } from "react-bootstrap";
 
 function OrderStatus() {
+  const navigator = useNavigate();
+  const image_url =
+    "https://www.cookwithmanali.com/wp-content/uploads/2019/05/Paneer-Butter-Masala.jpg";
+  const dish_name = "Paneer Tikka Masala";
+  const quantity = "2";
+  const order_status = "Confirmed";
   return (
     <>
       <h1 align="center">Your order details!</h1>
-      <OrderElement />
+      <Grid>
+        <OrderElement
+          sx={{ border: 1 }}
+          image_url={image_url}
+          dish_name={dish_name}
+          quantity={quantity}
+          order_status={order_status}
+        />
+        <OrderElement
+          image_url="https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg"
+          dish_name="Burger"
+          quantity="2"
+          order_status="Delivered"
+        />
+        <Button
+          size="small"
+          onClick={() => {
+            navigator(-1);
+          }}
+        >
+          Go back
+        </Button>
+      </Grid>
     </>
   );
 }
