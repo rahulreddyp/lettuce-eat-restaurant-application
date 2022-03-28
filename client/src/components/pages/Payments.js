1f9a07caab9c3fd19f16dfb54819f6c2daf811b1
Rahul Moje <rahulmoje@dal.ca>
2022-03-27 22:32:23 -0300

    Payments feature

--- client/src/components/pages/Payments.js
+++ client/src/components/pages/Payments.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 import ApplyCoupon from '../paymentscomponents/ApplyCoupon';
+import ApplyCouponNew from '../paymentscomponents/ApplyCouponNew';
 import PaymentForm from '../paymentscomponents/PaymentForm';
 import PaymentsNew from './PaymentsNew';
 
@@ -16,19 +17,21 @@ const Payments = () => {
     }
 
     return (
-    //    <Container className='p-1'>
+        <div>
+       <Container className='p-1'>
            
-    //        <Row className='p-3'>
-    //            <Col className='md-4'><Button onClick = {redirectToNewCard}>Add new card</Button></Col>
-    //            <Col className='md-4'><div className='jumbotron'>Total Payment Amount - {totalAmount}</div></Col>
-    //            <Col className='md-4'><ApplyCoupon totalAmount={totalAmount} setTotalAmount={setTotalAmount} couponCode={couponCode} setCouponCode = {setCouponCode}/></Col>
-    //        </Row>
-    //        <Row className='p-3'>
-    //            <PaymentForm totalAmount={totalAmount} couponCode = {couponCode}/>
-    //        </Row>
+           <Row className='p-3'>
+               <Col className='md-4'><Button onClick = {redirectToNewCard}>Add new card</Button></Col>
+               <Col className='md-4'><div className='jumbotron'>Total Payment Amount - {totalAmount}</div></Col>
+               <Col className='md-4'><ApplyCouponNew totalAmount={totalAmount} setTotalAmount={setTotalAmount} couponCode={couponCode} setCouponCode = {setCouponCode}/></Col>
+           </Row>
+           <Row className='p-3'>
+               <PaymentForm totalAmount={totalAmount} couponCode = {couponCode}/>
+           </Row>
            
-    //    </Container>
+       </Container>
     <PaymentsNew/>
+    </div>
     )
 
 }
