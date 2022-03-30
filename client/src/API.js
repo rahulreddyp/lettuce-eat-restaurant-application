ff2de59a27f888a2237ce5d94354b98eaad36a8e
Rahul Reddy Puchakayala <rh201373@dal.ca>
2022-03-30 12:51:58 -0300

    updated API link

--- client/src/API.js
+++ client/src/API.js
@@ -1,2 +1,2 @@
-export const API = "http://localhost:5000"
-// "https://lettuce-eat-app-back.herokuapp.com"
+export const API = "https://lettuce-eat-app-back.herokuapp.com"
+// "http://localhost:5000"
