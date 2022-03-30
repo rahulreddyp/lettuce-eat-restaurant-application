739f76fd4edc896dad8ef440291e011984fb039b
dsareen <dk930654@dal.ca>
2022-03-30 00:03:10 -0300

    windowlocationadded

--- client/src/components/pages/WishlistCard.js
+++ client/src/components/pages/WishlistCard.js
@@ -37,7 +37,7 @@ const WishlistCard = ({ item }) => {
           deletemessage = data.message;
           navigate("/wishlist", { state: { deletemessage } });
         }
-      }) 
+      } ,window.location.reload(false)) 
     };
 
   const moveToCart = async () => {
