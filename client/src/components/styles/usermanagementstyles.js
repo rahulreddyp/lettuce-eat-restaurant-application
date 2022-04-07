b004e74454eac31913d78e0de1d1eafac3c1fdbd
Arpan Nayankumar Bhatt <ar205025@dal.ca>
2022-04-07 16:26:24 -0300

    Update

--- client/src/components/styles/usermanagementstyles.js
+++ client/src/components/styles/usermanagementstyles.js
@@ -2,10 +2,7 @@ import "styled-components";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-<<<<<<< HEAD
   height: 110vh;
-=======
->>>>>>> f6dc52d2ead3c89c29c68b3d17ed7b1323c139af
   width: 100%;
   font-size: large;
   background-color: white;
@@ -13,8 +10,7 @@ const Wrapper = styled.div`
   padding-bottom: 5cm;
 
   .Container {
-<<<<<<< HEAD
-    height:fit-content;
+    height: fit-content;
     position: relative;
     contain: layout inline-size;
     padding: 1cm;
@@ -23,21 +19,12 @@ const Wrapper = styled.div`
     box-shadow: 0px 10px 10px black;
     background-color: #fea95e;
 
-   
-    
     width: 60%;
     font-family: "Poppins", sans-serif;
   }
 
   .Container:hover {
     box-shadow: 0 0 10px #dd7230;
-=======
-    background-color: #fea95e;
-    width: 60%;
-    font-family: "Poppins", sans-serif;
-    box-shadow: 0 0 10px #dd7230;
-    padding: 20px;
->>>>>>> f6dc52d2ead3c89c29c68b3d17ed7b1323c139af
   }
 
   h3 {
@@ -49,15 +36,12 @@ const Wrapper = styled.div`
     height: 5cm;
     width: 5cm;
   }
-<<<<<<< HEAD
 
   .form {
     margin: auto;
     padding: 0.25cm;
     text-align: left;
   }
-=======
->>>>>>> f6dc52d2ead3c89c29c68b3d17ed7b1323c139af
 `;
 
 export default Wrapper;
